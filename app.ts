e0eb5bf3d763e1f450161c411ffa76394875cf55
Bruno Costa e Silva <brunocosta.1991@hotmail.com>
2025-03-12 22:04:03 -0300

    initialize project with TypeScript setup and basic arithmetic functions

---
+++ app.ts
@@ -0,0 +1,4 @@
+const soma = (a: number, b: number): void => {
+  console.log(a + b)
+}
+soma(1, 2)
---
+++ index.js
@@ -0,0 +1,7 @@
+const soma = (a, b) => {
+  console.log(a + b)
+}
+
+soma(2, 2)
+soma("Bruno ", "T4ste")
+soma("1", "1")
